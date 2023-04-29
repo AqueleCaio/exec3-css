40d87a92720a970a288268c5fc107431587f13eb
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-04-29 12:22:02 -0300

    .

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-let box_viewer = document.getElementById('viewer')
 const all_numbers = document.getElementById('iNumber')
-const click_one = document.getElementById('number_one')
-const click_two = document.getElementById('number_two')
-const click_three = document.getElementById('number_three')
-const click_four = document.getElementById('number_four')
-const click_five = document.getElementById('number_five')
-const click_six = document.getElementById('number_six')
-const click_seven = document.getElementById('number_seven')
-const click_eight = document.getElementById('number_eight')
-const click_nine = document.getElementById('number_nine')
-
-
-click_one.addEventListener('click', insert)
-
+const delete_button = document.getElementById('delete')
+let box_viewer = document.getElementById('viewer')
 
 function insert(number){
-    box_viewer.innerHTML = number
-}
+    let num = box_viewer.innerHTML;   
+    box_viewer.innerHTML = `<div style="position: relative;
+                                                               left: 90%;
+                                                               font-size: 55px;
+                                                               color: white;
+                                                               display: inline-block;"> ${number}
+                                                            </div>
+                                                            `          
+};
+
+delete_button.addEventListener('click', function(){
+    box_viewer.innerHTML = `<div style="position: relative;
+    left: 90%;
+    font-size: 55px;
+    color: white;
+    display: inline-block;"> ${'0'}
+ </div>
+ `       
+});
 
 
 ////////////////////////////////////////////////////////////////////////
 
+//num + number;
+
+/*
+`<div style="position: relative;
+                           left: 90%;
+                           font-size: 55px;
+                           color: white;">${num + number}
+                 </div>
+                    `
+*/
 
 
 /*
