1ecba9eec25d053e76fd67b91a2bacc9f32d9206
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-05-02 15:52:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const all_numbers = document.getElementById('iNumber')
 const delete_button = document.getElementById('delete')
 let box_viewer = document.getElementById('viewer')
+const click_one = document.getElementById('number_one')
 
 function insert(number){
     let num = box_viewer.innerHTML;   
     box_viewer.innerHTML = `<div style="position: relative;
-                                                               left: 90%;
-                                                               font-size: 55px;
-                                                               color: white;
-                                                               display: inline-block;"> ${number}
-                                                            </div>
-                                                            `          
+                                        left: 90%;
+                                        font-size: 55px;
+                                        color: white;
+                                        display: inline-block;"> ${number}
+                                    </div>
+                                    `          
 };
 
 delete_button.addEventListener('click', function(){
@@ -22,49 +23,3 @@ delete_button.addEventListener('click', function(){
  </div>
  `       
 });
-
-
-////////////////////////////////////////////////////////////////////////
-
-//num + number;
-
-/*
-`<div style="position: relative;
-                           left: 90%;
-                           font-size: 55px;
-                           color: white;">${num + number}
-                 </div>
-                    `
-*/
-
-
-/*
-all_numbers.addEventListener('click', function(){
-    box_viewer.innerHTML = `<div 
-                            style="position: relative;
-                                   left: 90%;
-                                   font-size: 55px;
-                                   color: white;">${1} 
-                            </div>
-                            `
-})
-*/
-
-/* if (document.box_viewer == null){
-        box_viewer.innerHTML = `<div style="
-                                position: relative;
-                                left: 90%;
-                                font-size: 55px;
-                                color: white;"> ${1} 
-                            </div>
-                            `
-    }else{
-        box_viewer.innerHTML += `<div style="
-                                position: relative;
-                                left: 90%;
-                                font-size: 55px;
-                                color: white;"> ${1} 
-                            </div>
-                            `            
-    }
-    */
